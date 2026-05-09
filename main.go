98dc937528235834bb3e011b61dec5c373f13d7c
Madhasoeki <ramadhan.kpj@gmail.com>
2026-05-09 22:53:28 +0700

    feat: setup database infrastructure and environment variables

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"atsar/config"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: File .env tidak ditemukan")
+	}
+
+	config.ConnectDatabase()
+
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run()
+}
+
